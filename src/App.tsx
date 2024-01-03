b422f8b82c7c6f17b3f3099d570b3bdfba59722c
Tomás Espinosa <40611089+TomGaleano@users.noreply.github.com>
2024-01-03 14:41:21 +0000

    Creé test.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
 import { Home } from './components/home'
 import { Login } from './components/login'
 import { Register } from './components/register'
+import { Test } from './components/test'
 
 function App() {
   return (
     <Router>
       <Routes>
+          <Route path="/test" element={<Test/>} />
           <Route path="/home" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
---
+++ src/components/test.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+export interface LoginProps {
+    className?: string;
+    children?: React.ReactNode;
+}
+
+export const Test = ({
+    className = "",
+    children = null,
+}: LoginProps) => {
+    const [userData, setUserData] = useState({
+        first_name: "",
+        last_name: "",
+        email: "",
+        password: "",
+        phone: ""
+    });
+
+    const navigate = useNavigate();
+
+    const handleChange = (e: { target: { name: any; value: any; }; }) => {
+        setUserData({
+            ...userData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = async (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/login`, userData,{
+                withCredentials: true});
+            console.log(response.data);
+            localStorage.setItem("user", JSON.stringify(response?.data?.user));
+            navigate("/home");
+        } catch (error) {
+            console.error("Error during login:", error);
+        }
+    };
+
+    return (
+        <div className={`text-center ${className}`}>
+            <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', maxWidth: '300px', margin: '0 auto' }}>
+                <label>
+                    Email:
+                    <input name="email" type="email" value={userData.email} onChange={handleChange} placeholder="Email" />
+                </label>
+                <label>
+                    Password:
+                    <input name="password" type="password" value={userData.password} onChange={handleChange} placeholder="Password" />
+                </label>
+                <button type="submit" style={{ marginTop: '20px' }}>Inicia sesión</button>
+            </form>
+            {children}
+        </div>
+    );
+};
