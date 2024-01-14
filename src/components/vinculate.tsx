76e0a25e728c8f0699d7fe4ca6f63ae5a4d95b3c
Ownnie <nico.calderon.correa@gmail.com>
2024-01-14 09:35:11 -0500

    Se agregaron estilos y responsividades, ademas de la api de google maps y un componente de maps iframe

--- src/components/vinculate.tsx
+++ src/components/vinculate.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import "./Vinculate.css";
-import MapComponent from "./MapComponent.tsx"; // Importa el componente MapComponent
+import MapComponent from "./MapComponentAPI"; // Importa el componente MapComponent
 
 export const Vinculate = (): JSX.Element => {
+
+    const locations = [ // Define las 4 locaciones
+        { position: { lat: 4.687747682024201, lng: -74.13019486345699 } },
+        { position: { lat: 4.684118366151688, lng: -74.12388295108285 } },
+        { position: { lat: 4.68393689986349, lng: -74.11629651794085 } },
+        { position: { lat: 4.701962320882152, lng: -74.11605375208032 } },
+    ];
+
     return (
-        <div className='element_container'> para
+        <div className='element_container'>
             <div className='img_container'>
                 <h1>Trabaja con Nostros!</h1>
             </div>
             <h2 className='vinculate_title'>Formulario de Vinculación</h2>
             <div className='vinculate__container'>
-                <MapComponent /> {/* Usa el componente MapComponent */}
+                <MapComponent locations={locations} /> {/* Usa el componente MapComponent */}
                 <form className='vinculate_form' action="http://localhost:5173/nico.calderon.correa@gmail.com" method="POST">
                     <input type="text" id="name" name="name" placeholder="Nombre" required />
                     <input type="email" id="email" name="email" placeholder="Correo" required />
