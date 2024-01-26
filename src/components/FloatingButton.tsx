cb468e8b5f9e11e6d0c44c25db716e87c69901a8
TOMAS ESTEBAN BRICEÑO BERNAL <110701704+TEBB27@users.noreply.github.com>
2024-01-26 15:31:04 +0000

    comentario de errores en consola

--- src/components/FloatingButton.tsx
+++ src/components/FloatingButton.tsx
@@ -1,2 +1,20 @@
 import './FloatingButton.css'
 
+import React from 'react'
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
+
+const FloatingButton: React.FC = () => {
+  return (
+    <FloatingWhatsApp
+      phoneNumber={"+573016666666"}
+      accountName={'Tomas'}
+      allowEsc
+      chatMessage="Hi, I'm interested in your product!"
+      className="custom-class-name"
+      headerTitle="WhatsApp Chat"
+      zIndex={999}
+    />
+  )
+}
+
+export default FloatingButton
