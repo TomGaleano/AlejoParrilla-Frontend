ffa8e7500d7ddf8573cea8986c855ca23392133d
TOMAS ESTEBAN BRICEÑO BERNAL <110701704+TEBB27@users.noreply.github.com>
2024-01-29 15:25:59 +0000

    adicion de loadingPage

---
+++ src/components/loadingPage.tsx
@@ -0,0 +1,21 @@
+import './loadingPage.css';
+const loadingPage = () => {
+  return (
+    <div className="loader">
+        <div className="loader_item"></div>
+        <div className="loader_item"></div>
+        <div className="loader_item"></div>
+        <div className="loader_item"></div>
+        <div className="loader_item"></div> 
+        <div className="loader_item"></div>
+        <div className="loader_item"></div>
+        <div className="loader_item"></div>
+        <div className="loader_item"></div>
+        <div className="loader_item"></div>
+        <div className="loader_item"></div>
+        <div className="loader_item"></div>
+    </div>
+  );
+};
+
+export default loadingPage;
