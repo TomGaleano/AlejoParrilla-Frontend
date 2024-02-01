8691d08b5452da8ddfae46a6a1a5601917b8a20c
Tomás Espinosa <tom.ga.es@gmail.com>
2024-01-31 20:53:47 -0500

    applied redesign to Inicio and part of Nosotros

--- src/components/inicio.tsx
+++ src/components/inicio.tsx
@@ -1,7 +1,6 @@
 import "./Styles.css";
 import restaurant from '../assets/Nosotros-Fachada-AlejoParrilla.png';
 import parrilla from '../assets/parrila.png';
-import MenuGrid from './menu.tsx';
 
 
 export const Inicio = (): JSX.Element => {
@@ -9,15 +8,14 @@ export const Inicio = (): JSX.Element => {
 
     return (
         <div>
-            <div className="home_container landing">
+            <div className="container landing">
                 <div className="column-main">
                     <div>
-                        <h1 className="title">Bienvenido a AlejoParrilla</h1>
-                        <h3 className="subtitle">La mejor calidad desde 1999</h3>
+                        <h1 className="title">Tu parrilla, AlejoParilla</h1>
                     </div>
 
                     <div className="button-container">
-                        <button className="main-button">Menú</button>
+                        <button className="main-button">Menú en línea</button>
                         <button className="second-button">Pide ya</button>
                     </div>
                 </div>
@@ -26,27 +24,38 @@ export const Inicio = (): JSX.Element => {
                 </div>
             </div>
 
-            <div className="home_container">
+            <div className="container second">
+                <div className="column">
+                    <h2 className='title_alt'>Conoce sobre nosotros</h2>
+                    <div className="line">
+                        <p>
+                            Nuestro menu tiene diferentes variedades de platos distribuidos entre carnes, pescados, comidas rapidas, bolws y demas.
+                        </p>
+                        <button className="third-button">Conoce más</button>
+                    </div>
+
+                </div>
+                <div className="column has-image">
+                    <img src={parrilla} alt='Parrilla yummy' className="restaurant_image" />
+                </div>
+            </div>
+
+            <div className="container third">
                 <div className="column has-image">
                     <img src={parrilla} alt='Parrilla yummy' className="restaurant_image" />
                 </div>
                 <div className="column">
-                    <h2 className='title_alt'>Nuestra Filosofia</h2>
+                    <h2 className='title_alt'>Conoce sobre nosotros</h2>
                     <div className="line">
-                        <p>
-                            La filosofía de AlejoParilla es ofrecer comida deliciosa, servicio amable y ambiente agradable.
+                        <p style={{color:'white'}}>
+                            Nuestro menu tiene diferentes variedades de platos distribuidos entre carnes, pescados, comidas rapidas, bolws y demas.
                         </p>
-                        <p>
-                            En AlejoParilla creemos que la comida es más que una necesidad, es una forma de compartir momentos especiales.
-                        </p>
-                        <p>
-                            Nuestro objetivo es que nuestros clientes se sientan como en casa cuando visitan nuestros restaurantes.
-                        </p></div>
+                        <button className="third-button">Conoce más</button>
+                    </div>
 
                 </div>
             </div>
-            <div className='wave_spacer white-brown'></div>
-            <MenuGrid />
+
         </div>
     );
 };
--- src/components/sobre-nosotros.tsx
+++ src/components/sobre-nosotros.tsx
@@ -6,32 +6,36 @@ import Carousel from "./carousel";
 export const Sobrenosotros = (): JSX.Element => {
   return (
     <div style={{ overflow: 'hidden' }}>
-      <div style={{ paddingTop: '10vh' }}>
+      <div style={{ paddingTop: '5vh' }} className="landing">
         {/* Primera sección */}
         <h1 className='title tit-sobre-nosotros'>Sobre Nosotros</h1>
 
-        <div className='home_container' style={{ paddingTop: '1.3vh'}}>
+        <div className='container' style={{ paddingTop: '1.3vh'}}>
           {/* Inicia la primera columna de la sección */}
 
           <div className="sobre-nosotros">
-            <p >
-            Ofrecemos una amplia variedad de platos para todos los gustos, elaborados con ingredientes frescos y de calidad. Nuestro personal está altamente capacitado para brindar un servicio de calidad y atención personalizada. Nuestros restaurantes tienen una decoración moderna y acogedora, que crea un ambiente agradable para disfrutar de una buena comida.
+            <p>
+            Somos restaurantes especializados en Parrilla y platos típicos colombianos desde 1999. 
             </p>
-            <p style={{ marginBottom: '0vh' }}>Utilizamos los mejores insumos y la mejor atención para hacer que su experienciaen nuestros restaurantes sea única.</p>
+            <p>
+            Nuestro personal cuenta con una amplia experiencia y un gusto exquisito en la preparación de nuestro menú. 
+            </p>
+            <p style={{ marginBottom: '0vh' }}>Utilizamos los mejores insumos y la mejor atención para hacer que su experienciaen nuestros restaurantes sea única.
+            </p>
             <div>
-              <div className='home_container sobre-nosotros' style={{ display: 'flex', paddingTop: '1vh', marginTop: '0vh' }}>
+              <div className='container sobre-nosotros' style={{ display: 'flex',padding:'0vh', paddingTop: '1vh', paddingLeft: '8vw', marginTop: '0vh' }}>
                 {/* Espacio debajo del texto para poner cifras del restaurante */}
 
                 <div className="insights" style={{ textAlign: 'center' }}>
-                  <h2 className='title' style={{ paddingTop: '0vh' }}>4+</h2>
+                  <h2 className='subtitle' >4+</h2>
                   <p style={{ paddingLeft: '0vh' }}>Restaurantes</p>
                 </div>
                 <div className="insights" style={{ textAlign: 'center' }}>
-                  <h2 className='title' style={{ paddingTop: '0vh' }}>35+</h2>
+                  <h2 className='subtitle' >35+</h2>
                   <p style={{ paddingLeft: '0vh' }}>Platos</p>
                 </div>
                 <div className="insights" style={{ textAlign: 'center' }}>
-                  <h2 className='title' style={{ paddingTop: '0vh' }}>54+</h2>
+                  <h2 className='subtitle' >54+</h2>
                   <p style={{ paddingLeft: '0vh' }}>Empleados</p>
                 </div>
               </div>
@@ -47,7 +51,7 @@ export const Sobrenosotros = (): JSX.Element => {
 
 
 
-      <div className='home_container' style={{display:'flex', flexDirection:'column', backgroundImage:'../assets/fondo-azul-de-insignias.png'}}>
+      <div className='container' style={{display:'flex', flexDirection:'column', backgroundImage:'../assets/fondo-azul-de-insignias.png'}}>
          
             <h2 className="title">Nuestra Misión</h2>
 
@@ -73,7 +77,7 @@ export const Sobrenosotros = (): JSX.Element => {
 
       {/* Tercera Sección */}
       <div style={{  width: '100vw' }}>
-        <div className="home_container" style={{ backgroundColor: 'white', marginTop: '0vh', marginBottom: '0vh'}}>
+        <div className="container" style={{ backgroundColor: 'white', marginTop: '0vh', marginBottom: '0vh'}}>
           <h2 className='title tit-sobre-nosotros'>Nuestra Filosofía</h2>
           <div className="sobre-nosotros"style={{}}>
             <p style={{ color: 'black' }}>Somos restaurantes especializados en Parrilla y platos típicos colombianos desde 1999. </p>
@@ -82,7 +86,7 @@ export const Sobrenosotros = (): JSX.Element => {
           </div>
         </div>
       </div>
-      <div className="home_container" style={{paddingTop:'0vh', marginTop:'0vh', marginBottom:'15vh', paddingLeft:'0px', paddingRight: '0px'}}>
+      <div className="container" style={{paddingTop:'0vh', marginTop:'0vh', marginBottom:'15vh', paddingLeft:'0px', paddingRight: '0px'}}>
         <div className="column" style={{padding: 0}}>
           <h2 className="menu_title" style={{marginBottom:'4vh'}}>Nuestros restaurantes</h2>
           <Carousel />
