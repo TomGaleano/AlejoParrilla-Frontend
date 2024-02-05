c883d286e9019e21a0fe0856954499e9f46e69da
Tomás Espinosa <tom.ga.es@gmail.com>
2024-02-05 15:43:24 -0500

    fixes and adjustments

--- src/components/newMenu.tsx
+++ src/components/newMenu.tsx
@@ -19,33 +19,33 @@ function NewMenu() {
             </div>
             <div className={`primera_pagina ${currentPage === 'primera_pagina' ? 'show' : 'hide'}`}>
                 <div className="bento_menu_1_top">
-                    <div className="big_square">
+                    <div className="big_square bento_square">
                         <h3 className="bento_title">
                             Hamburguesas
                         </h3>
                         <FontAwesomeIcon className="bento_arrow" icon={faCircleChevronRight} />
                     </div>
-                    <div className="small_square">
+                    <div className="small_square bento_square">
                         <h3 className="bento_title">
                             Bowls
                         </h3>
                         <FontAwesomeIcon className="bento_arrow" icon={faCircleChevronRight} />
                     </div>
-                    <div className="small_square">
+                    <div className="small_square bento_square">
                         <h3 className="bento_title">
                             Rápidas
                         </h3>
                         <FontAwesomeIcon className="bento_arrow" icon={faCircleChevronRight} />
                     </div>
                 </div>
-                <div className="bento_menu_1_bottom">
+                <div className="bento_menu_1_bottom bento_square">
                     <div className="small_square">
                         <h3 className="bento_title">
                             Entradas
                         </h3>
                         <FontAwesomeIcon className="bento_arrow" icon={faCircleChevronRight} />
                     </div>
-                    <div className="long_square"> {/* Eso de geometría no es lo mío */}
+                    <div className="long_square bento_square"> {/* Eso de geometría no es lo mío */}
                         <h3 className="bento_title">
                             Sopas
                         </h3>
@@ -55,26 +55,26 @@ function NewMenu() {
             </div>
 
             <div className={`segunda_pagina ${currentPage === 'segunda_pagina' ? 'show' : 'hide'}`}>
-                <div className="bento_menu_2">
+                <div className="bento_menu_2 bento_square">
                     <div className="tall_square"> {/* No pregunten que es un tall square, solo déjenme sólo */}
                         <h3 className="bento_title">
                             Bebidas
                         </h3>
                         <FontAwesomeIcon className="bento_arrow" icon={faCircleChevronRight} />
                     </div>
-                    <div className="small_square_2">
+                    <div className="small_square_2 bento_square">
                         <h3 className="bento_title">
                             Típicos
                         </h3>
                         <FontAwesomeIcon className="bento_arrow" icon={faCircleChevronRight} />
                     </div>
-                    <div className="small_square_2">
+                    <div className="small_square_2 bento_square">
                         <h3 className="bento_title">
                             Postres
                         </h3>
                         <FontAwesomeIcon className="bento_arrow" icon={faCircleChevronRight} />
                     </div>
-                    <div className="big_square_2">
+                    <div className="big_square_2 bento_square">
                         <h3 className="bento_title">
                             Ensaladas
                         </h3>
