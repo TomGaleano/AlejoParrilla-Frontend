7515c8d3d072e874177a4a7160307f8b468315c1
Luis Nicolas Calderon Correa <ownnie@MacBook-de-Luis.local>
2024-02-05 09:47:31 -0500

    Limpieza de codigo (ya no esta pegado con babas)

--- src/components/sobre-nosotros.tsx
+++ src/components/sobre-nosotros.tsx
@@ -3,59 +3,59 @@ import Carousel from "./carousel";
 
 export const Sobrenosotros = (): JSX.Element => {
   return (
-    <div style={{ overflow: 'hidden' }}>
-      <div style={{ paddingTop: '5vh', minHeight:'100vh' }} className="landing">
+    <div className="overflow-hidden_SN">
+      <div className="padding-top-min-height_SN landing">
         {/* Primera sección */}
-        <h1 className='title tit-sobre-nosotros' style={{margin:'.2rem',fontSize:'5vh'}}>Sobre Nosotros</h1>
-        <div className="sobre-nosotros-container" style={{display:'flex', flexWrap:'wrap-reverse', justifyContent:'space-evenly', alignItems:'stretch'}}>
-          <div className="sobre-nosotros-container-item" style={{width:'50%',minWidth:'250px', color:'#ffffff'}}>
-            <div className="" style={{fontSize:'1rem', padding:'3rem',paddingTop:'0px', textAlign:'justify'}}>
+        <h1 className='title tit-sobre-nosotros margin-font-size_SN'>Sobre Nosotros</h1>
+        <div className="sobre-nosotros-container flex-wrap-justify-align_SN">
+          <div className="sobre-nosotros-container-item width-min-width-color_SN">
+            <div className="font-size-padding-text-align_SN">
               <h1 className="subtitle">Nuestra Filosofía</h1>
               <p>¡Bienvenidos a nuestros restaurantes! Disfruta de la mejor parrilla y platos típicos colombianos desde 1999. Ofrecemos gastronomía de calidad, con recetas tradicionales y sabores auténticos.</p>
               <p>Nuestro personal con amplia experiencia prepara un menú completo: carnes, sopas, ensaladas, postres y bebidas típicas. Ambiente acogedor, decoración agradable y servicio profesional.</p>
               <p>Utilizamos los mejores insumos, frescos y naturales, y la mejor atención para hacer que tu experiencia en nuestros restaurantes sea única e inolvidable. Te invitamos a visitarnos y a comprobar por qué somos los mejores en Parrilla y platos típicos colombianos. ¡Te esperamos!</p>
-              <div style={{display:'flex', alignItems:'center', justifyContent:'space-evenly',}}>
-              <div className="info-card" style={{}}>
-                <h2 className="subtitle">4+</h2>
-                <p>Restaurantes</p>
+              <div className="flex-align-justify_SN">
+                <div className="info-card">
+                  <h2 className="subtitle">4+</h2>
+                  <p>Restaurantes</p>
+                </div>
+                <div className="info-card">
+                  <h2 className="subtitle">35+</h2>
+                  <p>Platos</p>
+                </div>
+                <div className="info-card">
+                  <h2 className="subtitle">54+</h2>
+                  <p>Empleados</p>
+                </div>
               </div>
-              <div className="info-card" style={{}}>
-                <h2 className="subtitle">35+</h2>
-                <p>Platos</p>
-              </div>
-              <div className="info-card" style={{}}>
-                <h2 className="subtitle">54+</h2>
-                <p>Empleados</p>
-              </div>
-            </div>
             </div>
           </div>
 
 
-          <div className="sobre-nosotros-container-item" style={{boxSizing: 'border-box', width:'50%', padding:'2rem'}}>
-            <img style={{width:'100%', height:'auto', margin:'auto', borderRadius:'20px', border:'var(--color-our-orange) 2px solid'}} src='./images/Imagen-interior.webp' alt="" />
+          <div className="sobre-nosotros-container-item box-sizing-width-padding_SN">
+            <img className="img-width-height-margin-border_SN" src='./images/Imagen-interior.webp' alt="" />
           </div>
         </div>
 
-        
+
       </div>
       {/* Segunda Sección */}
 
 
 
-      <div className='container' style={{display:'flex', flexDirection:'column', background:'red', alignItems:'center'}}>
+      <div className='container flex-direction-background-align_SN'>
         <h2>Misión</h2>
 
       </div>
       {/* Tercera Sección */}
 
       <div className="container landing">
-        <div className="" style={{display:'flex', flexDirection:'row'}}>
-          <div className="" style={{border:'red 2px solid', background:'#ffffff'}}>
+        <div className="flex-direction-row_SN">
+          <div className="border-background_SN">
             <div className="">
               <img src="" alt="" />
             </div>
-            <p className="" style={{textAlign:'justify'}}>En Alejo Parrilla, aspiramos a ser el destino culinario de referencia, liderando la industria gastronómica con inovación y tradición en cada plato.</p>
+            <p className="text-align_SN">En Alejo Parrilla, aspiramos a ser el destino culinario de referencia, liderando la industria gastronómica con inovación y tradición en cada plato.</p>
           </div>
           <div className="">
             <div className="">
@@ -79,20 +79,17 @@ export const Sobrenosotros = (): JSX.Element => {
       </div>
 
       {/* Cuarta Sección */}
-      <div style={{  width: '100vw' }}>
-        <div className="container" style={{ backgroundColor: 'white', marginTop: '0vh', marginBottom: '0vh'}}>
+      <div className="width_SN">
+        <div className="container background-color-margin-top-bottom_SN">
           <h2 className='title tit-sobre-nosotros'>Nuestra Filosofía</h2>
-          <div className="sobre-nosotros"style={{}}>
-
-
-
+          <div className="sobre-nosotros">
 
           </div>
         </div>
       </div>
-      <div className="container" style={{paddingTop:'0vh', marginTop:'0vh', paddingLeft:'0px', paddingRight: '0px'}}>
-        <div className="column landing" style={{padding: 0}}>
-          <h2 className="menu_title" style={{marginBottom:'4vh'}}>Nuestros restaurantes</h2>
+      <div className="container padding-top-margin-top-left-right_SN">
+        <div className="column landing" style={{ padding: 0 }}>
+          <h2 className="menu_title margin-bottom_SN">Nuestros restaurantes</h2>
           <Carousel />
         </div>
       </div>
