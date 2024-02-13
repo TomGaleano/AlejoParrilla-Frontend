f35ab3ee4e9d7cefee6d0780317cbd455415d682
TOMAS ESTEBAN BRICEÑO BERNAL <110701704+TEBB27@users.noreply.github.com>
2024-02-13 17:57:10 -0500

    cambios carrusel

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,57 +1,18 @@
-import React from 'react';
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-
 const Carousel = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  };
-
   return (
-    <Slider {...settings}>
-      <div>
-        Deta
+    <section id="tranding">
+      <div className="container">
+        <h3 className="text-center section-subheading">Popular Delivery</h3>
+        <h1 className="text-cent section-heading">Trending food</h1>
+        <div className="container">
+          <div className="swiper tranding-slider">
+            <div className="swiper-wraper">
+              {/*Slide Start*/}
+            </div>
+          </div>
+        </div>
       </div>
-      <div>
-        <h3>Slide 2</h3>
-      </div>
-      <div>
-        <h3>Slide 3</h3>
-      </div>
-      {/* Agrega más diapositivas según sea necesario */}
-    </Slider>
+    </section>
   );
 }
-
 export default Carousel;
